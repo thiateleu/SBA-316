2e4f54f20d146e84bd5b5476d12f7d9229f7a839
thiateleu <thiateleu@gmail.com>
2024-07-14 00:49:06 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# thiateleu cleaning website
+
+is the single page shows the menu bar ang register side
--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-// Menu data structure
+/////////// Menu data structure
 var menuLinks = [
   { text: "about", href: "/ABOUT" },
   { text: "services", href: "/SERVICES" },
@@ -6,56 +6,64 @@ var menuLinks = [
   { text: "login", href: "/LOGIN" },
 ];
 
-//step 1
+//////////// cache the main element
 const mainEl = document.querySelector("main");
 console.log(mainEl);
-//step 2
+
 mainEl.style.backgroundColor = "var(--main-bg)";
-//let backGroundColor = 'var(--main-bg)'
-//step 3
-mainEl.innerHTML = "<h1>DOM Manipulation</h1>";
-//step 4
+/////////// let backGroundColor = 'var(--main-bg)'
+
+mainEl.innerHTML = "<h1>THIATELEU CLEANING</h1>";
 mainEl.classList = "flex-ctr";
 
-// Creating a menu bar
-//step 1
+//////////////// Creating a menu bar
+
 const topmenuEl = document.getElementById("top-menu");
 console.log(topmenuEl);
-// step 2
+
 topmenuEl.style.height = "100%";
-// step 3
+
 topmenuEl.style.backgroundColor = "var(--top-menu-bg)";
-//step 4
+
 topmenuEl.classList = "flex-around";
 
-//Adding Menu Buttons
-//step 1
+/////////////////// Adding Menu Buttons
+
 menuLinks.forEach(function (link) {
-  //step 2
   let linkEl = document.createElement("a");
-  //step 3
+
   linkEl.setAttribute("href", link.href);
-  //step 4
+
   linkEl.textContent = link.text;
-  //step 5
+
   topmenuEl.appendChild(linkEl);
 });
 
-// Part 2
+////////////// Creating the Submenu
 
-// Creating the Submenu
-//step 1
 const subMenuEl = document.getElementById("sub-menu");
 console.log(subMenuEl);
-//step 2
+
 subMenuEl.style.height = "100%";
-//step 3
+
 subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
-//step 4
+
 subMenuEl.classList = "flex-around";
 
-//submenu
-//step 1
 subMenuEl.style.position = "absolute";
-//step 2
+
 subMenuEl.style.top = "0";
+
+//////////////// Register event listeners for form validation
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (event) => {
+  const name = form.elements["name"].value;
+  const email = form.elements["email"].value;
+  const password = form.elements["password"].value;
+
+  if (name === "" || email === "" || password === "") {
+    alert("All field are requiered");
+    event.preventDefault();
+  }
+});
